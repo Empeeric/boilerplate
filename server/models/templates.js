cdaa58c3dc9ea4ede35c08a5fedead08987918fb
Alon Valadji <alon@ronin.co.il>
2013-12-26 01:31:53 +0200

    templates model

--- server/models/templates.js
+++ server/models/templates.js
@@ -1,10 +1,28 @@
 var mongoose = require('mongoose'),
     Types = mongoose.Schema.Types,
+    models = require('./'),
     views = require('../../front/views/');
 
+
 var schema = new mongoose.Schema({
-    mame: {type: String, enum: views, default: 'index'},
-    models: [{ type: Types.ObjectId, ref: 'models' }]
+    mame: { type: String, enum: views, default: 'index' },
+    models: [{
+        name: { type: String, enum: Object.keys(models) },
+        options: {
+            limit: Number,
+            paginate: { type: Boolean, default: false },
+            navigation: { type: Boolean, default: true },
+            sort: [{
+                key: String,
+                value: { type: String, enum: ['asc', 'desc'] }
+            }],
+            where: [{
+                key: String,
+                value: Types.Mixed
+            }],
+            populate: [String]
+        }
+    }]
 });
 
 schema.methods.toString = function(){
