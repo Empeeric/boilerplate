4040b1e8f55fff1ca52f2a29b38161b796e2fb53
etaypere <etaypere@gmail.com>
2013-12-30 17:19:57 +0200

    generalize grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,21 +5,8 @@ module.exports = function(grunt) {
                 separator: ';'
             },
             dist: {
-                src: [
-                    'public/resources/js/jquery-1.7.1.min.js',
-                    'public/resources/js/css_browser_selector.js',
-                    'public/resources/js/jquery.restjson.js',
-                    'public/resources/js/jquery.serializeobject.js',
-                    'public/resources/js/jquery.cycle.all.min.js',
-                    'public/resources/js/jquery.ui.core.min.js',
-                    'public/resources/js/jquery.ui.widget.min.js',
-                    'public/resources/js/jquery.ui.tabs.min.js',
-                    'public/resources/js/jquery.tools.min.js',
-                    'public/resources/js/jquery.pagination.js',
-                    'public/resources/js/blk.jquery.placeholder.js',
-                    'public/resources/js/script.js'
-                ],
-                dest: 'public/resources/js/built.js'
+                src: [],
+                dest: 'public/js/built.js'
             }
         },
 
@@ -29,7 +16,7 @@ module.exports = function(grunt) {
             },
             my_target: {
                 files: {
-                    'public/resources/js/built.min.js': ['public/resources/js/built.js']
+                    'public/js/built.min.js': ['public/js/built.js']
                 }
             }
         },
@@ -37,11 +24,8 @@ module.exports = function(grunt) {
         cssmin: {
             combine: {
                 files: {
-                    'public/resources/css/buit.min.css': [
-                        'public/resources/css/screen.css',
-                        'public/resources/css/typography.css',
-                        'public/resources/css/cross-browser.css',
-                        'public/resources/css/emp.css'
+                    'public/css/built.min.css': [
+                        'public/css/*.css'
                     ]
                 }
             }
